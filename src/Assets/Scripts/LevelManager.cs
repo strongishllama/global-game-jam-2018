51d949d00271d5da82f978e9e99cd13c8d4d38db
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2018-01-26 21:56:26 +1100

    Added main menu and level manager

---
+++ src/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public const string levelOneSceneName = "Level 1";
+
+    public static LevelManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+            Debug.Log(gameObject + " singleton instance was destroyed because a second one was created.");
+        }
+    }
+}
---
+++ src/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(LevelManager.levelOneSceneName);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
