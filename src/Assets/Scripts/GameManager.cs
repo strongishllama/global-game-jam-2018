baa77b4b3c14deafdeef92609f477ce42a9a8a8a
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2018-01-27 10:40:22 +1100

    Player ui work.

---
+++ src/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool gameIsRunning = false;
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log(gameObject.name + " singleton instance was destroyed because a second one was created.");
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        gameIsRunning = true;
+    }
+
+    public bool GameIsRunning
+    {
+        get
+        {
+            return gameIsRunning;
+        }
+        set
+        {
+            gameIsRunning = value;
+        }
+    }
+}
---
+++ src/Assets/Scripts/PlayerCanvas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCanvas : MonoBehaviour
+{
+    private decimal timeElapsed = 0;
+
+    [SerializeField]
+    private Text timeDisplay;
+
+    private void Update()
+    {
+        if (GameManager.instance.GameIsRunning)
+        {
+            timeElapsed += (decimal)Time.deltaTime;
+            timeDisplay.text = "Time: " + Math.Round(timeElapsed, 1, MidpointRounding.ToEven);
+        }
+    }
+}
