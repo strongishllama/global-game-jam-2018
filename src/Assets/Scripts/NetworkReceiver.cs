63ad3652d43be913f1c3fbf89953ac9f75b77dfd
Louis Nguyen <nguyenlouis4@gmail.com>
2018-01-26 22:03:27 +1100

    nothing achieved

---
+++ src/Assets/Scripts/NetworkReceiver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkReceiver:NetworkBehaviour {
+
+    NetworkClient mClient;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.anyKey) {
+            
+        }
+    }
+}
+
+
+public class InputMessageType {
+    public static short Input = MsgType.Highest + 1;
+}
+
+
