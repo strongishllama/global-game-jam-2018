1d542695710da03d7178d4d1434f9d8a4da56016
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2018-01-27 13:03:48 +1100

    Main menu updates.

--- src/Assets/Scripts/GameManager.cs
+++ src/Assets/Scripts/GameManager.cs
@@ -19,11 +19,6 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private void Start()
-    {
-        gameIsRunning = true;
-    }
-
     public bool GameIsRunning
     {
         get
