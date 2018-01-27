c669d7927f079e0abe598d20a34c011477f7e7ed
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2018-01-27 15:50:02 +1100

    Level updates

--- src/Assets/Scripts/PlayerCanvas.cs
+++ src/Assets/Scripts/PlayerCanvas.cs
@@ -4,13 +4,31 @@ using UnityEngine.UI;
 
 public class PlayerCanvas : MonoBehaviour
 {
+    [SerializeField]
+    private int maxLapCount = 3;
+    private int currentLapCount = 1;
+
     private decimal timeElapsed = 0;
 
     [SerializeField]
     private Text timeDisplay;
+    [SerializeField]
+    private Text lapDisplay;
+
+    public static PlayerCanvas instance;
 
     private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log(gameObject.name + " singleton instance was destroyed because a second one was created.");
+            Destroy(gameObject);
+        }
+
         if (timeDisplay == null)
         {
             Debug.Log("Warning:" + gameObject.name + " does not have an time display assigned.");
@@ -21,8 +39,25 @@ public class PlayerCanvas : MonoBehaviour
     {
         if (GameManager.instance.GameIsRunning)
         {
-            timeElapsed += (decimal)Time.deltaTime;
-            timeDisplay.text = "Time: " + Math.Round(timeElapsed, 1, MidpointRounding.ToEven);
+            UpdateTimeDisplay();
+            UpdateLapDisplay();
         }
+    }
+
+    private void UpdateTimeDisplay()
+    {
+        timeElapsed += (decimal)Time.deltaTime;
+        timeDisplay.text = "Time: " + Math.Round(timeElapsed, 1, MidpointRounding.ToEven);
+    }
+
+    private void UpdateLapDisplay()
+    {
+        lapDisplay.text = "Lap: " + currentLapCount + "/" + maxLapCount;
+    }
+
+    public void IncrementLapCount()
+    {
+        ++currentLapCount;
+        UpdateLapDisplay();
     }
 }
