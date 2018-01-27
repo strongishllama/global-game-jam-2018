26c6b5760635ccb96b1817dddc7a8db6548d4d6d
James Owen <jamesowen012@gmail.com>
2018-01-27 11:46:26 +1100

    OffRoad collision now done by wheel position

--- src/Assets/Scripts/WallCollision.cs
+++ src/Assets/Scripts/WallCollision.cs
@@ -6,12 +6,15 @@ using UnityEngine.Serialization;
 
 public class WallCollision : MonoBehaviour {
 
-    //public List<Transform> m_WheelPositions;
+    public Transform m_WheelHolderPosition;
 
     public Material m_Map;
 
     [Range(0, 10)]
     public float m_RaycastRange = 5;
+
+    [Range(0, 4)]
+    public int m_WheelsAllowedOffRoad = 3;
 
     public UnityEvent m_LeaveRoadEvent;
 
@@ -22,13 +25,24 @@ public class WallCollision : MonoBehaviour {
 
     void Awake() {
         m_LeaveRoadEvent.AddListener(LeaveRoadTest);
+
+        if(m_WheelHolderPosition.childCount == 0) {
+            Debug.LogWarning("m_WheelHolderPosition has no children.");
+        }
     }
 
     // Update is called once per frame
     void Update() {
 
         bool previousOnRoad = m_CurrentlyOnRoad;
-        m_CurrentlyOnRoad = TestPoint(transform.position);
+        int hits = 0;
+        for(int i = 0; i < m_WheelHolderPosition.childCount; i++) {
+            if (TestPoint(m_WheelHolderPosition.GetChild(i).position)) {
+                hits++;
+            }
+        }
+
+        m_CurrentlyOnRoad = hits < (m_WheelHolderPosition.childCount - m_WheelsAllowedOffRoad);
 
         if (m_CurrentlyOnRoad) {
             if (previousOnRoad != m_CurrentlyOnRoad) {
@@ -41,7 +55,7 @@ public class WallCollision : MonoBehaviour {
     /// checks to see if a point is on the road or not
     /// </summary>
     /// <param name="a_Position">Position to test</param>
-    /// <returns>true if off the road, false if on the road</returns>
+    /// <returns>false if off the road, true if on the road</returns>
     private bool TestPoint(Vector3 a_Position) {
         //could replace m_Map with the material from the hit transform
         RaycastHit hit;
@@ -55,10 +69,10 @@ public class WallCollision : MonoBehaviour {
             float color = col.r + col.g + col.b;
 
             if (color <= 0.01f) {
-                return false;
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
     private void LeaveRoadTest() {
