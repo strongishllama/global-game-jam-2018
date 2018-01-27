d75bfac4181ebf38cd6e18b85eb344f8d05be258
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2018-01-28 10:58:22 +1100

    Added splash screen

--- src/Assets/Scripts/MainMenu.cs
+++ src/Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void Play()
     {
-        SceneManager.LoadScene(LevelManager.instance.GetCurrentLevel().lvlName);
+        SceneManager.LoadScene("Level 1");
     }
 
     public void Quit()
