b2182c8eb4f53b69e0d22402a5db807fe8c86400
James Owen <jamesowen012@gmail.com>
2018-01-28 11:43:46 +1100

    missing class, called after wining the round

---
+++ src/Assets/Scripts/LoadMainMenuAfterTime.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadMainMenuAfterTime : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 5);
+	}
+
+
+    void OnDestroy() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
